554a6be4e443b62f561da2ac1ed0c67e9b7ad16c
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-02-12 20:23:56 +0100

    template arguments

--- main.go
+++ main.go
@@ -16,30 +16,38 @@ func main() {
 	for _, f := range flag.Args() {
 		state := newState(f)
 		outFname := NoExt(f)
-		state.check(ioutil.WriteFile(outFname, []byte(state.Render(f)), 0666))
+		if outFname == f{
+			log.Println("skipping", f, ": input and output file are the same")
+			continue
+		}
+		output := []byte(state.Inc(f))
+		if state.err == nil{
+			state.check(ioutil.WriteFile(outFname, output, 0666))
+		}
 	}
 }
 
 func newState(rootFile string) *State{
-	return &State{Root: rootFile, File: rootFile, vars: make(map[string]interface{})}
+	return &State{Root: rootFile, File: rootFile }
 }
 
 // a *State is passed to template rendering
 type State struct {
 	File, Root string
-	Args []interface{}
-	vars map[string]interface{}
+	args []interface{}
+	err error
 }
 
 // Render recursively expands the template in fname.
-func (s *State) Render(fname string, args ...interface{}) string {
-	content := s.Read(fname)
+func (s *State) Inc(fname string, args ...interface{}) string {
+	content := s.Raw(fname)
 	t := template.Must(template.New(fname).Parse(content))
 	out := bytes.NewBuffer(nil)
 
 	prev := *s
 
 	s.File = fname
+	s.args = args
 	s.check(t.Execute(out, s))
 
 	*s = prev
@@ -47,25 +55,33 @@ func (s *State) Render(fname string, args ...interface{}) string {
 	return out.String()
 }
 
-func (s *State) Def(key string, value interface{}) string {
-	s.vars[key] = value
-	return "" // must return something to template
-}
 
-func (s *State) Var(key string) interface{} {
-	if v, ok := s.vars[key]; ok {
-		return v
-	} else {
-		log.Println("warning: undefined variable", key, "in", s.File)
-		return ""
-	}
-}
+// TODO: scoping
+//func (s *State) Def(key string, value interface{}) string {
+//       s.vars[key] = value
+//       return "" // must return something to template
+//}
+//
+//func (s *State) Var(key string) interface{} {
+//       if v, ok := s.vars[key]; ok {
+//               return v
+//       } else {
+//               log.Println("warning: undefined variable", key, "in", s.File)
+//               return ""
+//       }
+//}
+
+
 
 // Read expands to the raw contents of fname without rendering the file.
-func (s *State) Read(fname string) string {
+func (s *State) Raw(fname string) string {
 	bytes, err := ioutil.ReadFile(fname)
 	s.check(err)
 	return string(bytes)
+}
+
+func(s*State)Arg(index int)interface{}{
+	return s.args[index]
 }
 
 // Remove extension from file name.
@@ -76,6 +92,7 @@ func NoExt(file string) string {
 
 func (s *State) check(err error) {
 	if err != nil {
-		log.Fatal("error in ", s.File, ": ", err)
+		log.Println("error in ", s.Root, "->", s.File, ": ", err)
+		s.err = err
 	}
 }
